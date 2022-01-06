7a3fc28eabae3a18742d510087a46facab981b86
cynthia <yinnycfu@gmail.com>
2022-01-06 13:20:46 +0000

    chore: create css file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
+// import css file
+import "./index.css";
+
 import NavBar from "./App";
 
 ReactDOM.render(<NavBar />, document.getElementById("root"));
