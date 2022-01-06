f6b6b19272b0fbecbd8776a43b40a38cf8891554
cynthia <yinnycfu@gmail.com>
2022-01-06 17:20:18 +0000

    feat: render Product as child for ShoppingCart component

---
+++ src/ShoppingCart.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+export default class ShoppingCart extends Component {
+  state = {
+    products: [
+      { id: 1, productName: "A", price: 1, quantity: 0 },
+      { id: 2, productName: "B", price: 2, quantity: 0 },
+      { id: 3, productName: "C", price: 3, quantity: 0 },
+      { id: 4, productName: "D", price: 4, quantity: 0 },
+      { id: 5, productName: "E", price: 5, quantity: 0 },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>Shopping Cart</h4>
+
+        <div>
+          {this.state.products.map((product) => {
+            return <Product key={product.id}/>;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
