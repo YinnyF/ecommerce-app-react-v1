ef53a917c5392b188f2402bc1e2ea3bb6dcc5b7f
cynthia <yinnycfu@gmail.com>
2022-01-06 14:25:25 +0000

    style: add style to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,26 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello</h1>
-        <p>paragraph</p>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">my ecommerce app</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+
+              </ul>
+
+            </div>
+          </div>
+        </nav>
       </React.Fragment>
     );
   }
