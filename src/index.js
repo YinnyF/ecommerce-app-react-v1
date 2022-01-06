c6518424c39ca71ac1bb00ac95994adf3cc63e6c
cynthia <yinnycfu@gmail.com>
2022-01-06 12:36:29 +0000

    chore: set up bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// to set up bootstrap
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
 var element = <div>Hello World</div>;
 ReactDOM.render(element, document.getElementById("root"));
-
-console.log(element);
