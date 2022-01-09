2083249c916a1b55b61a973e25bb874179b7dcaa
cynthia <yinnycfu@gmail.com>
2022-01-09 22:52:21 +0000

    feat: use react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
 import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <BrowserRouter>
         <NavBar />
-        <Login />
-      </React.Fragment>
+
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/customers" exact element={<CustomersList />} />
+          <Route path="/cart" exact element={<ShoppingCart />} />
+        </Routes>
+
+      </BrowserRouter>
     );
   }
 }
